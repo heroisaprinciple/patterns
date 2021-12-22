6b7aa53a265b0b56150bb8bdd911dd9ecc6fc6cd
heroisaprinciple <valcat552@gmail.com>
2021-12-22 21:45:26 +0200

    coming back to life

---
+++ patterns_7/proxy.js
@@ -0,0 +1,104 @@
+class AACCT { //@abstract class
+    constructor() {}
+    getBalance() {
+        throw new Error('No implementation');
+    }
+    withdraw() {
+        throw new Error('No implementation');
+    }
+}
+
+class ATMController {
+    constructor() {}
+    handleBalanceRequest(balance) {
+        return balance;
+    }
+    handleLogin(account_id) {
+        if (account_id === new AccountDB().logIn()) { // TODO here: if account_id === AccountDB object.login()
+           return true;
+        }
+        else { return false; }
+    }
+    handleLogout() {}
+    handleWithDrawal() {
+        if (new AccountProxy().withdraw()) {
+            return true;
+        }
+        else { 'Hehe, I will give you no money'};
+    }
+}
+
+class IDB extends AACCT { //abstract class
+    constructor() {
+        super();
+    }
+    logIn() {
+        throw new Error('No implementation');
+    }
+    logOut() {
+        throw new Error('No implementation');
+    }
+}
+
+class Account extends AACCT {
+    constructor() {
+        super();
+    }
+    getBalance(balance) {
+        return balance;
+    }
+    withdraw() {
+    }
+}
+
+class AccountDB extends IDB {
+    constructor(account_id) {
+        super();
+        this.account_id = account_id;
+    }
+    logIn(account_id) {
+        return account_id;
+    }
+    logOut() {
+        if (new AccountProxy().withdraw()) {
+            return 'The operation finished. Logged out.';
+        }
+    }
+}
+
+class AccountProxy extends AACCT {
+    constructor(balance) {
+        super();
+        this.balance = balance;
+    }
+    getBalance() {
+        return this.balance;
+    }
+    withdraw() {
+        if (this.balance > 0) {
+            return true;
+        }
+        else { return 'Your balance is null'}
+    }
+}
+
+class DBProxy extends IDB {
+    constructor() {
+        super();
+    }
+    logIn(account_id) {
+        return account_id;
+    }
+    logOut() {
+        if (new AccountProxy().withdraw()) {
+            return 'The operation finished. Logged out.'
+        }
+    }
+    makeAccountProxy() {
+        new AccountProxy();
+    }
+}
+
+let acc_proxy = new AccountProxy();
+let db_proxy = new DBProxy();
+
---
+++ patterns_7/strategy.js
@@ -0,0 +1,91 @@
+class Piece { //@abstractclass
+    constructor(positions = [], color, num_of_moves, move_rule) {
+        this.positions = positions;
+        this.color = color; // either black or white
+        this.num_of_moves = num_of_moves;
+        this.move_rule = move_rule;
+
+        if (this.constructor === Piece) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+    }
+    move(new_positions = []) {
+        return {};
+    }
+    static checkPositionRange(positions) { // static method
+        return;
+    }
+    printInfo() {
+        return null;
+    }
+}
+
+class Knight extends Piece{
+    constructor() {
+        super();
+    }
+    move() {
+        return knight_move;
+    }
+}
+
+class Bishop extends Piece {
+    constructor() {
+        super();
+    }
+    move() {
+        return bishop_move;
+    }
+}
+
+//@abstractclass
+//also a context
+class MoveRule {
+    constructor(move_rule) {
+        this.move_rule = move_rule;
+
+        if (this.constructor === MoveRule) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+    }
+    //@abstractmethod
+    getAllMoves() {
+        throw new Error('You have to implement the method...');
+    }
+}
+
+//strategy 1
+class KnightMove extends MoveRule {
+    positions = [];
+    constructor(move_rule) {
+        super();
+        this.move_rule = move_rule;
+    }
+    getAllMoves() {
+        console.log(this.positions);
+    }
+}
+
+//strategy 2
+class BishopMove extends MoveRule {
+    positions = [];
+    constructor(move_rule) {
+        super();
+        this.move_rule = move_rule;
+    }
+    getAllMoves() {
+        console.log(this.positions);
+    }
+}
+
+// let move_rule = new MoveRule(); // leads to an error
+
+
+let knight_move = new KnightMove('move multiple squares');
+let knight = new Knight(knight_move);
+
+let bishop_move = new BishopMove(' move diagonally');
+let bishop = new Bishop();
+
+knight_move.getAllMoves();
+bishop_move.getAllMoves();
